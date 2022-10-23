a4740cf26b8b656ed0b0210c9d04b3e5987ec931
Wesley Santos <wesleymarcosm@gmail.com>
2022-10-23 08:13:56 -0300

    feat: Ajuste de estrutura de navegação.

--- exercicio.php
+++ exercicio.php
@@ -6,25 +6,28 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
-    <title>Curso PHP</title>
+    <link rel="stylesheet" href="assets/css/exercicio.css">
+    <title>Exercicio</title>
 </head>
-<body>
+<body class="exercicio">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
-        <h2>Índice dos Exercícios</h2>
+        <h2>Visualização do Exercício</h2>
     </header>
+    <nav class="navegacao">
+        <a href=<?="{$_GET['dir']}/{$_GET['file']}.php" ?> class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho" >Voltar</a>
+    </nav>
     <main class="principal">
         <div class="conteudo">
-            <nav class="modulos">
-                <div class="modulo verde">
-                    <h3>Módulo 01</h3>
-                    <ul>
-                        <li><a href="exercicio.php?dir=test&file=teste">Exercício A</a></li>
-                    </ul>
-                </div>
-            </nav>
+            <?php
+                /* include($_GET['dir']."/".$_GET['file'].".php"); */
+                include(__DIR__."/{$_GET['dir']}/{$_GET['file']}.php");
+            ?>
         </div>
     </main>
-    <footer class="rodape">COD3R & ALUNOS © <?= date('Y'); ?></footer>
+    <footer class="rodape">
+        COD3R & ALUNOS © <?= date('Y'); ?>
+    </footer>
 </body>
 </html>
