502a966b936a214368d696dd838df6546874f4bd
Wesley Santos <wesleymarcosm@gmail.com>
2022-10-23 21:40:58 -0300

    feat: finalização do módulo 3.

---
+++ basico/desafio.php
@@ -0,0 +1,7 @@
+<div class="titulo">Desafio</div>
+<h5>Resulver de 3 formar diferentes</h5>
+<ul>
+    <li>1 + 1 = <?= 1+1 ?></li>
+    <li>4 + 4 = <?php echo 4 + 4; ?></li>
+    <li>8 + 8 = <?= "1"?><?php echo 3+3?></li>
+</ul>
